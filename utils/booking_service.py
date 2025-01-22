7b52241c418c4255b7c39fe13424baef69f2077f
hamdy-farg <farghamdy72@gmail.com>
2025-01-23 01:48:30 +0200

    .bug fix in bookig availability

--- utils/booking_service.py
+++ utils/booking_service.py
@@ -50,8 +50,7 @@ class BookingService:
         bookings = BookModel.query.filter(
             BookModel.room_id == room.id,
             BookModel.date == date,
-            BookModel.status == StatusEnum.approved,
-            BookModel.status == StatusEnum.inProgress,
+            BookModel.status != StatusEnum.rejected,
         ).all()
 
         available_slots = [(default_start_time, default_end_time)]
